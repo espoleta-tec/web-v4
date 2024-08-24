326dc25ca1e9e1037b246d98457c51b8ce80e394
Lazaro O'Farrill <lazaroofarrill@gmail.com>
2024-08-24 13:19:25 -0400

    Updates

--- _app/immutable/chunks/paths.DaOUUJCP.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1xqi67t)==null?void 0:s.base)??"/web-v4";var t;const a=((t=globalThis.__sveltekit_1xqi67t)==null?void 0:t.assets)??e;export{a,e as b};
---
+++ _app/immutable/entry/start.DN3SBSwH.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bp37-5Ax.js";export{t as start};
