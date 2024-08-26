796b434e3750c2bc7784acffbd94ea1d22f23ed8
Lazaro O'Farrill <lazaroofarrill@gmail.com>
2024-08-26 11:23:21 -0400

    changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -46,11 +46,14 @@
         <Icon src={FiMenu} />{data.menu_text}
       </div>
     </div>
-    <ul
+    <div
+      role="link"
+      tabindex="0"
       class="dropdown-content w-auto min-w-64 absolute left-1/2 -translate-x-1/2 bottom-0 translate-y-full bg-gray-50 shadow-md p-4 text-center text-xl flex flex-col gap-4"
     >
-      {#each data.nav_links as nav_link}
+      {#each data.nav_links as nav_link, i}
         <a
+          tabindex={i + 1}
           data-sveltekit-reload
           href="/{locale}{nav_link.en.url}"
           class="hover:bg-green-800 hover:text-white p-2 hover:cursor-pointer"
@@ -58,14 +61,15 @@
           {nav_link[locale].title}
         </a>
       {/each}
-    </ul>
+    </div>
   </div>
   <div class="flex-grow"></div>
   <div
     class="uppercase hidden lg:flex lg:flex-row flex-col gap-4 items-center text-green-800 font-medium text-center"
   >
-    {#each data.nav_links as nav_link}
+    {#each data.nav_links as nav_link, i}
       <a
+        tabindex={i + 1}
         data-sveltekit-reload
         href="/{locale}{nav_link.en.url}"
         class="hover:bg-green-800 hover:text-white p-2 hover:cursor-pointer"
